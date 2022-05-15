82d0b4e23c23836c5e420ca13dc23034b0ee5d35
Nikolay_Davydov <skmoscow74@yandex.ru>
2022-05-15 13:10:33 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27. 
+//5 -> 1, 8, 27, 64, 125
+
+int[] CubArray(int n){
+if (n<1) return new int[0];
+int []array = new int[n];
+for (int i=1; i<=n; i++){
+array[i-1]= (int)Math.Pow(i,3);
+}
+return array;
+}
+Console.WriteLine("Введите целое число =>1");
+int n=int.Parse(Console.ReadLine());
+int [] array = CubArray(n);
+ Console.Write($"{n} -> ");
+for (int i=0; i<n; i++){
+    Console.Write(array[i] +", ");
+}
