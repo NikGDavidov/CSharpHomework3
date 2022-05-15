002014b5f79695ba84b82e0549d7d4a38f36286a
Nikolay_Davydov <skmoscow74@yandex.ru>
2022-05-15 12:45:29 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double Distance(int x1,int y1,int z1, int x2, int y2,int z2 ){
+    return Math.Round(Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2) + Math.Pow(z2-z1,2)),4);
+}
+
+ Console.WriteLine($"Расстояние между точками {Distance(4,3,4,6,8,9)}");
